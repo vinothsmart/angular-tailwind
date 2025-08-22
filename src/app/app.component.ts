0c291e2d88d527888a4d4b3aa97aeabb5929191a
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-22 15:14:07 +0530

    updated color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,18 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Welcome to {{ title }}!</h1>
+    <p>This is a simple Angular application styled with Tailwind CSS.</p>
 
     <router-outlet />
   `,
-  styles: [],
+  styles: [
+    `
+      p {
+        background-color: #f3f4f6;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'angular-tailwind';
