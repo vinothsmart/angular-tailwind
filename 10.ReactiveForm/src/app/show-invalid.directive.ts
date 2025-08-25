1e5328a9303088e45b3690bbb270c016d36c5d17
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-25 19:35:49 +0530

    validation

--- 10.ReactiveForm/src/app/show-invalid.directive.ts
+++ 10.ReactiveForm/src/app/show-invalid.directive.ts
@@ -1,8 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[appShowInvalid]',
 })
 export class ShowInvalidDirective {
-  constructor() {}
+  constructor(private el: ElementRef, private control: NgControl) {}
+
+  @HostListener('blur')
+  onBlur() {
+    if (this.control.invalid) {
+      this.el.nativeElement.classList.add('is-invalid');
+    } else {
+      this.el.nativeElement.classList.remove('is-invalid');
+    }
+  }
+
+  // we can reuse this directive for other form controls
+  @HostListener('input')
+  onInput() {
+    if (this.control.invalid) {
+      this.el.nativeElement.classList.add('is-invalid');
+    } else {
+      this.el.nativeElement.classList.remove('is-invalid');
+    }
+  }
 }
