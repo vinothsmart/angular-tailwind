1da4f1704f5a43f60c086c962bf893689d0f0954
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-25 13:30:03 +0530

    name to age form buider

--- 10.ReactiveForm/src/app/app.component.ts
+++ 10.ReactiveForm/src/app/app.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = '10.ReactiveForm';
+  userForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'
+          ),
+        ],
+      ],
+      address: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        zip: ['', [Validators.required, Validators.pattern('^[0-9]{5}$')]],
+      }),
+      age: ['', [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  submitForm() {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    }
+  }
 }
