f1cfbdb4b5305dd81ada23ccf5dc18d48ee3acb2
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-25 19:18:50 +0530

    ng generate directive showInvalid

---
+++ 10.ReactiveForm/src/app/show-invalid.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ShowInvalidDirective } from './show-invalid.directive';
+
+describe('ShowInvalidDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ShowInvalidDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ 10.ReactiveForm/src/app/show-invalid.directive.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appShowInvalid]',
+})
+export class ShowInvalidDirective {
+  constructor() {}
+}
