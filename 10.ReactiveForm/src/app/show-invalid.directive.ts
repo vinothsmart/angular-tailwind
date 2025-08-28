e77209edf4182ea98b61d575d519e5a0d68fcf86
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-28 11:36:42 +0530

    rxjs added

--- 10.ReactiveForm/src/app/show-invalid.directive.ts
+++ 10.ReactiveForm/src/app/show-invalid.directive.ts
@@ -1,47 +1,43 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { NgControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Directive({
   selector: '[appShowInvalid]',
 })
-export class ShowInvalidDirective {
+export class ShowInvalidDirective implements OnInit, OnDestroy {
+  private statusChangesSubscription!: Subscription;
   constructor(private el: ElementRef, private control: NgControl) {}
 
+  ngOnInit() {
+    this.statusChangesSubscription = this.control.statusChanges!.subscribe(
+      () => {
+        this.updateClass();
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.statusChangesSubscription.unsubscribe();
+  }
+
+  // // Listen for the 'blur' event, which occurs when a user leaves the input field.
   // @HostListener('blur')
   // onBlur() {
-  //   if (this.control.invalid) {
-  //     this.el.nativeElement.classList.add('is-invalid');
-  //   } else {
-  //     this.el.nativeElement.classList.remove('is-invalid');
-  //   }
+  //   this.updateClass();
   // }
 
-  // // we can reuse this directive for other form controls
+  // // Listen for the 'input' event, which fires on every keystroke.
   // @HostListener('input')
   // onInput() {
-  //   if (this.control.invalid) {
-  //     this.el.nativeElement.classList.add('is-invalid');
-  //   } else {
-  //     this.el.nativeElement.classList.remove('is-invalid');
-  //   }
+  //   this.updateClass();
   // }
-
-  ngOnInit() {
-    // Check initial state on component load, in case a value was pre-filled and is invalid.
-    this.updateClass();
-  }
-
-  // Listen for the 'blur' event, which occurs when a user leaves the input field.
-  @HostListener('blur')
-  onBlur() {
-    this.updateClass();
-  }
-
-  // Listen for the 'input' event, which fires on every keystroke.
-  @HostListener('input')
-  onInput() {
-    this.updateClass();
-  }
 
   private updateClass() {
     // Check if the control is invalid AND has been interacted with (dirty or touched).
