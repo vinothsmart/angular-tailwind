255024909d69858bf38547669a2b34df11584d64
vinothsmart <s.vinothkanna90@gmail.com>
2025-09-02 18:15:20 +0530

    new angular

--- 1.intro/src/app/app.component.ts
+++ 1.intro/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
