b4ba3b1971a2cc98a308bb0803b52eaff0c4c269
vinothsmart <s.vinothkanna90@gmail.com>
2025-09-02 18:41:09 +0530

    *ngIf=

--- 2.StructureDirective/src/app/app.component.ts
+++ 2.StructureDirective/src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = '2.StructureDirective';
+  title = 'Structure Directives';
+
+  isLoggedIn: boolean = false;
+  userName: string = 'John Doe';
 }
