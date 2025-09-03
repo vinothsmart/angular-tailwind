e776d89320c02318e4d72980c960ef0b1224d2f1
vinothsmart <s.vinothkanna90@gmail.com>
2025-09-03 16:38:15 +0530

    upper and lowercase

--- 4.Pipes/src/app/app.component.ts
+++ 4.Pipes/src/app/app.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = '4.Pipes';
+  today = new Date();
 }
