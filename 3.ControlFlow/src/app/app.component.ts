3a98a9896076989ebfd3446c8a07698cb7e2b013
vinothsmart <s.vinothkanna90@gmail.com>
2025-09-03 13:35:49 +0530

    if elseif and else

--- 3.ControlFlow/src/app/app.component.ts
+++ 3.ControlFlow/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = '3.ControlFlow';
+
+  a: number = 2;
+  b: number = 3;
+  c: number = 4;
 }
