ead9a2c35d2679d3eb6b26ed99d239be93456904
vinothsmart <s.vinothkanna90@gmail.com>
2025-09-03 15:30:27 +0530

    @for

--- 3.ControlFlow/src/app/app.component.ts
+++ 3.ControlFlow/src/app/app.component.ts
@@ -12,4 +12,19 @@ export class AppComponent {
   a: number = 2;
   b: number = 3;
   c: number = 4;
+
+  items = [
+    {
+      id: 1,
+      name: 'Item 1',
+    },
+    {
+      id: 2,
+      name: 'Item 2',
+    },
+    {
+      id: 3,
+      name: 'Item 3',
+    },
+  ];
 }
