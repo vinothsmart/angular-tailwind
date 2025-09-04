0b99b396d33c30616f02357562f3d4d349a181c1
vinothsmart <s.vinothkanna90@gmail.com>
2025-09-04 15:29:37 +0530

    up

--- 15.lifecyclehook/src/app/header/header.component.ts
+++ 15.lifecyclehook/src/app/header/header.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent
+  implements
+    OnInit,
+    OnChanges,
+    OnDestroy,
+    DoCheck,
+    AfterViewInit,
+    AfterViewChecked,
+    AfterContentInit,
+    AfterContentChecked
+{
+  ngOnInit() {
+    console.log('Header component initialized');
+  }
 
+  ngOnChanges() {
+    console.log('Header component changes detected');
+  }
+  ngOnDestroy(): void {
+    console.log('Header component destroyed');
+  }
+  ngDoCheck(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngAfterViewChecked(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngAfterContentInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngAfterContentChecked(): void {
+    throw new Error('Method not implemented.');
+  }
 }
