470bab7220ffe212387a7f3d9e5615979d17c769
vinothsmart <s.vinothkanna90@gmail.com>
2025-09-11 12:50:30 +0530

    API integration

--- 5.DIServices/src/app/services/data.service.ts
+++ 5.DIServices/src/app/services/data.service.ts
@@ -1,12 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
+  private apiURL = 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) {}
+
   getData() {
     return ['Data 1', 'Data 2', 'Data 3'];
   }
 
-  constructor() {}
+  getPosts(): Observable<any> {
+    return this.http.get(this.apiURL);
+  }
 }
