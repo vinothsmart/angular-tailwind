5832e12adc06148086c3ab6a1ee9e2757af048f0
vinothsmart <s.vinothkanna90@gmail.com>
2025-09-11 12:23:48 +0530

    DI used in app

--- 5.DIServices/src/app/app.component.ts
+++ 5.DIServices/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
+  providers: [DataService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  data: string[] = [];
+
+  constructor(private dataService: DataService) {
+    this.data = this.dataService.getData();
+  }
+}
--- 5.DIServices/src/app/services/data.service.ts
+++ 5.DIServices/src/app/services/data.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  getData() {
+    return ['Data 1', 'Data 2', 'Data 3'];
+  }
 
-  constructor() { }
+  constructor() {}
 }
