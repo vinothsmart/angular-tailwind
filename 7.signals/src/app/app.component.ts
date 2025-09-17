0010317b3620312ee9c29aa4822e49e017db5edf
vinothsmart <s.vinothkanna90@gmail.com>
2025-09-17 16:46:29 +0530

    functions created

--- 7.signals/src/app/app.component.ts
+++ 7.signals/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Todo } from './interfaces/todo';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,34 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  count = signal<number>(0);
+  todos = signal<Todo[]>([]);
+  newTodo = signal<string>('');
+
+  ngOnInit() {
+    console.log(this.count());
+  }
+
+  handleInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.newTodo.set(target.value);
+  }
+
+  addTodo() {
+    const text = this.newTodo();
+    if (text) {
+      const newTodo: Todo = {
+        id: this.todos().length + 1,
+        text,
+        done: false,
+      };
+      this.todos.update((todos) => [...todos, newTodo]);
+      this.newTodo.set('');
+    }
+  }
+
+  deleteTodo(id: number) {
+    this.todos.update((todos) => todos.filter((todo) => todo.id !== id));
+  }
+}
